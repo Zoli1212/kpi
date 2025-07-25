e45c970d60e924d5977e67fdbe3f6d82727181c8
zoltan-molnar <rdigital4455@gmail.com>
2025-07-26 00:01:49 +0200

    noti01

--- src/components/header/NotificationDropdown.tsx
+++ src/components/header/NotificationDropdown.tsx
@@ -7,7 +7,7 @@ import { DropdownItem } from "../ui/dropdown/DropdownItem";
 
 export default function NotificationDropdown() {
   const [isOpen, setIsOpen] = useState(false);
-  const [notifying, setNotifying] = useState(true);
+  const [notifying, setNotifying] = useState(false);
 
   function toggleDropdown() {
     // setIsOpen(!isOpen);
